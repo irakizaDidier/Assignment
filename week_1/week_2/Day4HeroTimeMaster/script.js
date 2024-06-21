90e068506e2125ed654f2a40a33f8f20b05b2caf
irakizaDidier <didier.habimana@amalitechtraining.org>
2024-06-21 09:06:51 +0200

    Challenge 2: Object Oriented Clock &  Challenge 3: Time Formatting

--- week_1/week_2/Day4HeroTimeMaster/script.js
+++ week_1/week_2/Day4HeroTimeMaster/script.js
@@ -16,3 +16,41 @@ console.log("Seconds: ", seconds);
 
 // Challenge 2: Object Oriented Clock
 //Design a Clock object with properties like hours, minutes, and seconds
+
+function Clock() {
+    let currentTime = new Date();
+    // console.log("this is this: ", this)
+    this.hours = currentTime.getHours();
+    this.minutes = currentTime.getMinutes();
+    this.seconds = currentTime.getSeconds();
+}
+
+// Evaluation:
+let myClock = new Clock();
+console.log("Clock Instance: ", myClock);
+console.log("Hours: ", myClock.hours);
+console.log("Minutes: ", myClock.minutes);
+console.log("Seconds: ", myClock.seconds);
+
+// Challenge 3: Time Formatting
+// Add methods to the Clock object for formatting time.
+
+Clock.prototype.getFormattedTime = function () {
+    let hours = this.hours < 10 ? '0' + this.hours : this.hours;
+    let minutes = this.minutes < 10 ? '0' + this.minutes : this.minutes;
+    let seconds = this.seconds < 10 ? '0' + this.seconds : this.seconds;
+    return `${hours}:${minutes}:${seconds}`;
+};
+
+Clock.prototype.get12HourTime = function () {
+    let hours = this.hours % 12 || 12;
+    let minutes = this.minutes < 10 ? '0' + this.minutes : this.minutes;
+    let seconds = this.seconds < 10 ? '0' + this.seconds : this.seconds;
+    let period = this.hours >= 12 ? 'PM' : 'AM';
+    return `${hours}:${minutes}:${seconds} ${period}`;
+};
+
+//Evaluation:
+
+console.log("Formatted Time: ", myClock.getFormattedTime());
+console.log("12-Hour Format Time: ", myClock.get12HourTime());
