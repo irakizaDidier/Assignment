f56768c9262bf27da8517093dde8b40d8199a952
irakizaDidier <didier.habimana@amalitechtraining.org>
2024-06-22 20:29:19 +0200

    Add methods to the superhero object: usePower(powerName): Logs a message about the hero using a specific power. // revealIdentity(): Logs the hero’ s secret identity

--- week_1/JavaScript_Hero/script.js
+++ week_1/JavaScript_Hero/script.js
@@ -15,3 +15,18 @@ console.log(superhero);
 // 2. Methods and Functionality
 // Task: Add methods to the superhero object: usePower(powerName): Logs a message about the hero using a specific power.
 // revealIdentity(): Logs the hero’ s secret identity.
+
+superhero.usePower = function (powerName) {
+    if (this.powers.includes(powerName)) {
+        console.log(`${this.name} uses ${powerName}!`);
+    } else {
+        console.log(`${this.name} doesn't have the power ${powerName}.`);
+    }
+};
+
+superhero.revealIdentity = function () {
+    console.log(`The secret identity of ${this.name} is ${this.secretIdentity}.`);
+};
+
+superhero.usePower("Invisibility");
+superhero.revealIdentity();
