b0098816e71510cea34646a507431355142f97d5
irakizaDidier <didier.habimana@amalitechtraining.org>
2024-06-22 20:25:53 +0200

    Creation of an object representing a superhero

---
+++ week_1/JavaScript_Hero/script.js
@@ -0,0 +1,17 @@
+'use strict'
+
+//1. Object Creation Basics
+// Task: Create an object representing a superhero with properties like name, secretIdentity, powers (an array), and weakness.
+
+let superhero = {
+    name: "Shadow Avenger",
+    secretIdentity: "Irakiza Didier",
+    powers: ["Invisibility", "Super Strength", "Flight"],
+    weakness: "Kryptonite"
+};
+
+console.log(superhero);
+
+// 2. Methods and Functionality
+// Task: Add methods to the superhero object: usePower(powerName): Logs a message about the hero using a specific power.
+// revealIdentity(): Logs the hero’ s secret identity.
